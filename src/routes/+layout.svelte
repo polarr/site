d7715250df688c32fe6e0709ce6a88faf8c0a66a
polarity <Charles.Ran9@gmail.com>
2023-01-27 23:54:42 -0500

    feat: added footer, updated navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,45 +12,64 @@
 		Chevron,
 		Button,
 		DropdownDivider,
-		DarkMode
+		DarkMode,
+		Footer,
+		FooterLinkGroup,
+		FooterLink,
+		FooterBrand,
+		FooterIcon,
+		FooterCopyright
 	} from 'flowbite-svelte';
 
-	import { Sun, Moon } from 'svelte-heros-v2';
-
-	let theme = 'dark';
-
-	const toggleTheme = () => {
-		if (theme == 'dark') {
-			theme = 'light';
-		} else {
-			theme = 'dark';
-		}
-	};
+	import { GithubBrand, DiscordBrand, YoutubeBrand, InstagramBrand, SteamBrand, EnvelopeSolid } from 'svelte-awesome-icons';
 </script>
 
-<div class="app">
+<div class="app relative flex flex-col h-screen">
 	<Navbar let:hidden let:toggle>
 		<NavBrand href="/">
 			<img src="/mango_v3.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
 		</NavBrand>
-		<NavHamburger on:click={toggle} />
 		<div class="flex md:order-1">
 			<DarkMode />
 			<NavHamburger on:click={toggle} />
 		</div>
 		<NavUl {hidden}>
 			<NavLi href="/about">About</NavLi>
+			<NavLi href="/dev">Dev</NavLi>
+			<NavLi href="/projects">Research</NavLi>
 			<NavLi href="/projects">Projects</NavLi>
 			<NavLi href="/blog">Blog</NavLi>
 			<NavLi href="/resume">Resume</NavLi>
 		</NavUl>
 	</Navbar>
-	<main>
-		<slot />
-	</main>
+	<div class="grow overflow-scroll">
+		<main class="min-h-fit">
+			<slot />
+		</main>
+		<Footer class="p-6">
+			<FooterCopyright href="/" by="Polarity" copyrightMessage="Last Updated 2023-1-27" />
+			<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+				<FooterIcon href="/" class="text-gray-400 hover:text-gray-900">
+					<GithubBrand size="20"/>
+				</FooterIcon>
+				<FooterIcon href="/" class="text-gray-400 hover:text-gray-900">
+					<YoutubeBrand size="20"/>
+				</FooterIcon>
+				<FooterIcon href="/" class="text-gray-400 hover:text-gray-900">
+					<DiscordBrand size="20"/>
+				</FooterIcon>
+				<FooterIcon href="/" class="text-gray-400 hover:text-gray-900">
+					<SteamBrand size="20"/>
+				</FooterIcon>
+				<FooterIcon href="/" class="text-gray-400 hover:text-gray-900">
+					<EnvelopeSolid size="20"/>
+				</FooterIcon>
+			</div>
+		</Footer>
+	</div>
 </div>
 
-<style lang="scss">
+<style>
 	main {
 		width: 100%;
 		height: 100%;
