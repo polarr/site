905ccfcd00b9ae13ab3ad5c3fbabd1c5a85d46a3
polarity <Charles.Ran9@gmail.com>
2023-01-27 23:55:21 -0500

    feat: added base home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
 <script lang="ts">
-	import { Heading, P, Button } from 'flowbite-svelte';
+	import { Heading, P, Button, Span } from 'flowbite-svelte';
 </script>
+
+<div class="flex flex-col text-center justify-center min-h-full">
+	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl">
+		<Span gradient>Mango</Span>
+	</Heading>
+	<P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+		Software Engineer & Science enthusiast
+	</P>
+	<Button href="/">Learn more</Button>
+</div>
