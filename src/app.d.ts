f0c15f84e258a22d1f7d18eb4337a5b09145bbdd
polarity <Charles.Ran9@gmail.com>
2023-06-01 21:40:29 -0400

    chore: added type interface for achievements and projects

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,9 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { IconType } from "flowbite-svelte/types";
+import type { SvelteComponent, SvelteComponentTyped } from "svelte";
+import type { SvelteComponentDev } from "svelte/internal";
+
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -12,6 +17,35 @@ declare global {
 			logoColor?: string;
 			loved?: boolean;
 			color: string;
+		}
+
+		declare interface Achievement {
+			title: string;
+			date: string;
+			icon:  any;
+			description?: string;
+		}
+
+		declare interface ProjectLink {
+			icon: any;
+			label: string;
+			href: string;
+			iconRight?: boolean;
+		}
+
+		declare interface ProjectBadge {
+			name: string;
+			slug: string;
+			logoColor?: string;
+			color: string;
+		}
+
+		declare interface Project {
+			title: string;
+			description: string;
+			imageSrc: string;
+			links: ProjectLink[];
+			technologies: ProjectBadge[];
 		}
 	}
 }
