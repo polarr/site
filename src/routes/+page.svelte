27852d6e3ae40ab8ad40be5d7b06fa135f719877
polarity <Charles.Ran9@gmail.com>
2023-06-01 21:43:07 -0400

    feat: added landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
-	import { Heading, P, Button, Span } from 'flowbite-svelte';
+	import { Heading, A, P, Button, Span, FooterIcon } from 'flowbite-svelte';
+	import { DiscordBrand, EnvelopeSolid, GithubBrand, InstagramBrand, SteamBrand, YoutubeBrand } from 'svelte-awesome-icons';
 </script>
 
-<div class="flex flex-col text-center justify-center min-h-full">
+<div class="my-auto text-center min-h-full">
 	<Heading tag="h1" class="mb-4" customSize="text-4xl font-extrabold md:text-5xl lg:text-6xl">
-		<Span gradient>Mango</Span>
+		<Span gradient>Polarity</Span>
 	</Heading>
-	<P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-		Software Engineer & Science enthusiast
+	<P class="mb-6 text-center text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+		Software & Math Enthusiast
 	</P>
-	<Button href="/">Learn more</Button>
+	<div class="flex space-x-6 mb-4 justify-center">
+		<A href="/redirect/github" color="text-gray-400 hover:text-gray-900">
+			<GithubBrand size="24"/>
+		</A>
+		<A href="/redirect/youtube" color="text-gray-400 hover:text-gray-900">
+			<YoutubeBrand size="24"/>
+		</A>
+		<A href="/redirect/instagram" color="text-gray-400 hover:text-gray-900">
+			<InstagramBrand size="24" />
+		</A>
+		<A href="/redirect/discord" color="text-gray-400 hover:text-gray-900">
+			<DiscordBrand size="24"/>
+		</A> 
+		<A href="/redirect/steam" color="text-gray-400 hover:text-gray-900">
+			<SteamBrand size="24"/>
+		</A>
+		<A href="/redirect/email" color="text-gray-400 hover:text-gray-900">
+			<EnvelopeSolid size="24"/>
+		</A>
+	</div>
+	<Button href="/about" outline color="light">About Me</Button>
 </div>
