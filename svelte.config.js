f58b7e7bdd075df519f6ea0696b9fb11058f9e5f
polarity <Charles.Ran9@gmail.com>
2024-03-13 15:59:13 -0400

    feat: set up mdsvex, shiki, katex support

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,39 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex, escapeSvelte } from 'mdsvex'
+import { getHighlighter } from 'shiki';
+import rehypeKatexSvelte from "rehype-katex-svelte";
+import remarkMath from 'remark-math';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
+	extensions: ['.svelte', '.md', '.svx'],
 	preprocess: [
 		vitePreprocess(),
+		mdsvex({
+			layout: 'src/routes/blog/mdsvex/mdsvex.svelte',
+			extensions: ['.md', '.svx'],
+			remarkPlugins: [
+				remarkMath,
+			],
+			rehypePlugins: [
+				rehypeKatexSvelte,
+			],
+			highlight: {
+				highlighter: async (code, lang = 'text') => {
+					const highlighter = await getHighlighter({
+						themes: ['github-dark'],
+						langs: ['c', 'cpp', 'glsl', 'julia', 'python', 'javascript', 'typescript']
+					})
+					await highlighter.loadLanguage('c', 'cpp', 'glsl', 'julia', 'python', 'javascript', 'typescript')
+					const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'github-dark' }))
+					return `{@html \`${html}\` }`
+				}
+			}
+		}),
 		preprocess({
 			postcss: true
 		})
