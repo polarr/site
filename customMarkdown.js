ade6b69c0fc07c39332898ef08ad1a0717c266df
polarr <charles.ran9@gmail.com>
2025-02-20 22:43:36 -0800

    fix: custom blockquote math styling

--- customMarkdown.js
+++ customMarkdown.js
@@ -11,11 +11,7 @@ function customBlockquotes() {
 		visit(tree, 'text', (node, i) => {
 			const type = Object.keys(blockquoteTypes).find((key) => node.value.startsWith(key) || node.position.start.line == lastEdit);
 			if (type) {
-                if (node.position.start.line != lastEdit){
-                    node.value = `<span class=${blockquoteTypes[type]}>${node.value}</span>`;
-                } else {
-                    node.value = `<span class=${blockquoteTypes[type]}>::before${node.value}</span>`;
-                }
+                node.value = `<span class=${blockquoteTypes[type]}>${node.value}</span>`;
 
                 lastEdit = node.position.start.line;
 			}
