0b3943cc8c0763217a3d67c142f1ef90a76139f7
polarr <charles.ran9@gmail.com>
2025-02-20 22:26:47 -0800

    blog: update 5 and p1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -56,7 +56,7 @@
 		</main>
 		<Footer class="p-6">
 			<span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-				© 2020-{new Date().getFullYear()} • Last Updated 2025-01-04
+				© 2020-{new Date().getFullYear()} • Last Updated 2025-02-20
 			</span>
 			<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
 				<FooterIcon href="/redirect/github" class="text-gray-400 hover:text-gray-900">
